480d23ee941a8e9e0f6bb5d66c9783d3ac92c6a4
charanudari17 <charanudari835@gmail.com>
2026-06-03 14:29:31 +0530

    Update counter app

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(10)
 function check(){
-  if(count==0){
+  if(count<=0){
     // console.log("unable to decrement");
+    setCount(0);
     alert("not possilbe")
   }
   else{
@@ -16,8 +17,30 @@ function check(){
   }
 
 }
+function add10(){ 
+  setCount(count=>count+10)
+
+}
+function remove10(){ 
+  // setCount(count=>count-10)
+  if(count<=0){
+    // console.log("unable to decrement");
+    setCount(0);
+    alert("not possilbe")
+  }
+  else{
+    if( count-10<0)
+    {
+      alert("not possilbe")
+    }
+    else{
+    setCount(count=>count-10);
+    }
+  }
+
+}
   return (
-   <div className='h-screen  bg-sky-300 items-center flex flex-wrap justify-center gap-2'>
+   <div className='h-screen  bg-sky-300 items-center flex flex-col  justify-center gap-2'>
     
     <div className='bg-pink-200 rounded-2xl px-1 text-3xl'>counter:{count}</div>
    <div className=" flex  flex-wrap justify-center  gap-2 p-0 px-2 rounded-2xl"style={{backgroundColor:'orangered'}}> 
@@ -33,8 +56,13 @@ function check(){
 
     </button>
    </div>
+   <div className=' px2 gap-2.5 flex flex-wrap'>
+    <button onClick={add10} className='text-2xl text-white     px-2  bg-amber-300 border-2 rounded-2xl   '  > add 10</button>
+    <button onClick={remove10} className='text-2xl text-white  px-2  bg-pink-400 border-2 rounded-2xl   '  > remove10</button>
+
+   </div>
    </div>
   )
-}
+} 
 
 export default App
